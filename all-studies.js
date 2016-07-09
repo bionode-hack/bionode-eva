8df2ff54ea54c3edee3e0d6230fdb61c2ad9c9f7
MCarmen Jimenez Campos <matrimc@gmail.com>
2016-07-09 17:20:48 +0100

    get formatted data

--- all-studies.js
+++ all-studies.js
@@ -5,33 +5,34 @@ var JSONStream = require('JSONStream');
 var split = require('split2');
 var through = require('through2');
 
-function getIdsFromAllStudiesRequest (object, enc, next){
-	var noResults = object.response[0].result.length,
-		studiesResult = object.response[0].result,
-		i = 0,
-		studies = [];
-	for (i = 0; i < noResults; i++){
-		studies.push({
-			external_id: studiesResult[i].id,
-		});
-	}
-	this.push(studies);
-	next();
-};
-
 function getStudiesMetadata(taxonomies){
-	var getStudyIds = through.obj(getIdsFromAllStudiesRequest);
 	var urlStudiesAll = {   
 		protocol: 'http',   
 		host: 'www.ebi.ac.uk',   
 		pathname: '/eva/webservices/rest/v1/meta/studies/all'
 	};
 
-	request(url.format(urlStudiesAll))
-			.pipe(split())
-			.pipe(JSONStream.parse())
-			.pipe(getStudyIds)
-			.pipe(process.stdout);
+	requestPromise(url.format(urlStudiesAll))
+		.then(function(data){
+			var object = JSON.parse(data), 
+				resultLength = object.response[0].result.length,
+				results = object.response[0].result,
+				i,
+				studies = [],
+				baseUrl = 'ftp://ftp.ebi.ac.uk/pub/databases/eva/';
+			for (i = 0; i < resultLength; i++){
+				if (taxonomies[results[i].taxonomyId]){
+					studies.push({
+						external_id: results[i].id,
+						title: results[i].name,
+						description: results[i].description,
+						url: baseUrl + results[i].id + '/eva_normalised_files/',
+						tech: results[i].platform
+					});
+				}
+			}
+			console.log(studies);
+		});
 }
 
 var urlSpecies = {   
@@ -44,11 +45,14 @@ var taxonomies = {};
 
 requestPromise(url.format(urlSpecies))
 	.then(function(data){
-		var results = object.response[0].result,
+		var object = JSON.parse(data),
+			results = object.response[0].result,
 			resultLength = results.length,
 			i,
 			taxonomies = {};
 		for (i = 0; i < resultLength; i++) {
 			taxonomies[results[i].taxonomyId] = results[i].taxonomyCode;
 		}
+		console.log(taxonomies);
+		getStudiesMetadata(taxonomies);
 	});
